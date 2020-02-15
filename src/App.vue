23cdf13cc2a0a8e73b534a03731c39aff5281d1c
Elise <eliseknwls@gmail.com>
2020-02-15 17:14:26 +0100

    navbar

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,25 @@
 <template>
+
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Navbar />
+
+    <router-view></router-view>
   </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Navbar from './components/Navbar.vue'
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    Navbar,
+
   }
-}
+};
+
 </script>
 
 <style>
@@ -23,6 +29,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <button
+            class="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarText"
+            aria-controls="navbarText"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+            >
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarText">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <router-link class="nav-link" to='/'>Accueil </router-link>
+                </li>
+                <li class="nav-item">
+                    <router-link class="nav-link" to='/favoris'>Favoris </router-link>
+                </li>
+            </ul>
+            </div>
+        </nav>
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Navbar',
+}
+
+</script>
+
+
+<style scoped>
+router-link {
+  color: black;
+  cursor: pointer;
+}
+
+</style>
